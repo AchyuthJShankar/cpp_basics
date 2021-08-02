047735906791ab0da7529b8d8613fc18e3c03f96
Acchu <67947893+AchyuthJShankar@users.noreply.github.com>
2021-08-03 01:46:29 +0530

    Add files via upload

---
+++ 03Datatype.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age;
+    char gender;
+    string name;
+    float cgpa;
+    bool isPassed;
+    age=21;
+    gender='M'; //''used
+    name="Acchu";
+    cgpa=8.4;
+    isPassed=true; //for bool, 'true' or 'false' are only data.
+    cout<<age<<" "<<gender<<" "<<name<<" "<<cgpa<<" "<<isPassed;
+    return 0;
+}
