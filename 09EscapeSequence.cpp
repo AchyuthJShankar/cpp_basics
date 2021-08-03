255a4d99b15932134c4454af8fa25a6b6ba885d9
Acchu <67947893+AchyuthJShankar@users.noreply.github.com>
2021-08-04 01:06:19 +0530

    Add files via upload

---
+++ 09EscapeSequence.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    /*to print - Hi "Acchu"
+    if we use 
+        cout<<"Hi "Acchu"";
+    it shows error */
+
+    cout<<"Hi \"Acchu\""<<"\n"; //Escape func
+
+    cout<<"C:\\Acchu\\mnop.exe"; //used to print /(backslash)
+    return 0;
+}
