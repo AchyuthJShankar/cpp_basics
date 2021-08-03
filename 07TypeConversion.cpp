f4baa6ecf933405df41d2e7e2ce72087754d8ed7
Acchu <67947893+AchyuthJShankar@users.noreply.github.com>
2021-08-04 00:35:24 +0530

    Update 07TypeConversion.cpp

--- 07TypeConversion.cpp
+++ 07TypeConversion.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 int main(){
     int a=10, b=3;
-    double c= double(a)/b; //Explicit Conversion
-    cout<<c;
+    double c= double(a)/b; //Explicit Conversion in C style
+    double d=static_cast<double>(a)/b; //In Cpp style
+    cout<<c<<endl<<d;
     return 0;
 }
