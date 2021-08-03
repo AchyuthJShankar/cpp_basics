014b6c1f43e0d0c7f1db3bee107b0f5fa2dd0cc6
Acchu <67947893+AchyuthJShankar@users.noreply.github.com>
2021-08-03 23:47:07 +0530

    Add files via upload

---
+++ 05AreaCircle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+const double PI=3.14;
+int main(){
+    int r;
+    cout<<"Enter the radius"<<endl;
+    cin>>r;
+    cout<<"Area is "<<(PI*r*r);
+    return 0;
+
+}
