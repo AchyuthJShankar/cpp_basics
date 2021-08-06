abb30570682b1bc3ea94679362c49e74b8667054
Acchu <67947893+AchyuthJShankar@users.noreply.github.com>
2021-08-07 01:21:58 +0530

    Add files via upload

---
+++ 15Function.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+void func(){ //function declared
+    cout<<"FUNCTION CALLED"<<"\n";
+}
+int main(){
+    cout<<"function is being called"<<"\n";
+    func(); //fuction called
+    cout<<"DONE";
+    return 0;
+}
