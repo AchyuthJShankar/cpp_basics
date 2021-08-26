f8f1765651e2757307dee98f112958dac373f098
Acchu <67947893+AchyuthJShankar@users.noreply.github.com>
2021-08-26 22:50:28 +0530

    Add files via upload

---
+++ stringWithSpace.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string name;
+    cout<<"ENTER UR FULL NAME"<<"\n";
+    getline(cin, name); //Takes string input with spaces
+    cout<<"hi "<<name<<", how are you?";
+    return 0;
+}
